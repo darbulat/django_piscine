fd859e387b8cfd89331150c118a46a4074802266
ljarrahd <ljarrahd@student.school-21.ru>
2022-05-18 20:15:50 +0300

    ex01

--- day03/ex01/my_script.sh
+++ day03/ex01/my_script.sh
@@ -4,7 +4,5 @@ SMALL_PROGRAM="my_program.py"
 
 python3 -m pip --version
 
-python3 -m pip install --target local_lib --log log.log  git+https://github.com/jaraco/path.git &> /dev/null
+python3 -m pip install --target local_lib --log log.log --upgrade --force-reinstall git+https://github.com/jaraco/path.git &> /dev/null && python3 $SMALL_PROGRAM || echo "NOK"
 
-# execute the small program
-python3 $SMALL_PROGRAM
